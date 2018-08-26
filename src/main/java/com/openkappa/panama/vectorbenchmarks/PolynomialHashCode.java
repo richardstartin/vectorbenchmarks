01fb3131a3157da21278dc8485942003906e6ec6
Richard Startin <richard@openkappa.co.uk>
2018-08-26 13:06:56 +0100

    fix unrolled loop by going backwards

--- src/main/java/com/openkappa/panama/vectorbenchmarks/PolynomialHashCode.java
+++ src/main/java/com/openkappa/panama/vectorbenchmarks/PolynomialHashCode.java
@@ -21,17 +21,13 @@ public class PolynomialHashCode {
   private int[] coefficients;
   private int[] data;
   private int seed;
-  private static final int[] POWERS_OF_31 = new int[] {
-          1,
-          31,
-          31 * 31,
-          31 * 31 * 31,
-          31 * 31 * 31 * 31,
-          31 * 31 * 31 * 31 * 31,
-          31 * 31 * 31 * 31 * 31 * 31,
-          31 * 31 * 31 * 31 * 31 * 31 * 31,
-          31 * 31 * 31 * 31 * 31 * 31 * 31 * 31
-  };
+  private static final int[] POWERS_OF_31_BACKWARDS = new int[33];
+  static {
+    POWERS_OF_31_BACKWARDS[POWERS_OF_31_BACKWARDS.length - 1] = 1;
+    for (int i = POWERS_OF_31_BACKWARDS.length - 2; i >= 0; --i) {
+      POWERS_OF_31_BACKWARDS[i] = 31 * POWERS_OF_31_BACKWARDS[i + 1];
+    }
+  }
 
   public static void main(String[] args) {
     PolynomialHashCode hc = new PolynomialHashCode();
@@ -40,6 +36,7 @@ public class PolynomialHashCode {
     System.out.println(hc.arraysHashCode());
     System.out.println(hc.hashCodeAutoVectorised());
     System.out.println(hc.polynomialHashCode());
+    System.out.println(hc.polynomialHashCodeUnrolled());
   }
 
   @Setup(Level.Iteration)
@@ -69,17 +66,8 @@ public class PolynomialHashCode {
 
   @Benchmark
   public int polynomialHashCode() {
-    var next = YMM_INT.broadcast(POWERS_OF_31[8]);
-    var coefficients = YMM_INT.scalars(
-            31 * 31 * 31 * 31 * 31 * 31 * 31,
-            31 * 31 * 31 * 31 * 31 * 31,
-            31 * 31 * 31 * 31 * 31,
-            31 * 31 * 31 * 31,
-            31 * 31 * 31,
-            31 * 31,
-            31,
-            1
-    );
+    var next = YMM_INT.broadcast(POWERS_OF_31_BACKWARDS[33 - 9]);
+    var coefficients = YMM_INT.fromArray(POWERS_OF_31_BACKWARDS, 33 - 8);
     var acc = YMM_INT.zero();
     for (int i = data.length; i - YMM_INT.length() >= 0; i -= YMM_INT.length()) {
       acc = acc.add(coefficients.mul(YMM_INT.fromArray(data, i - YMM_INT.length())));
@@ -90,28 +78,26 @@ public class PolynomialHashCode {
 
   @Benchmark
   public int polynomialHashCodeUnrolled() {
-    var next = YMM_INT.broadcast(POWERS_OF_31[8]);
-    var coefficients1 = YMM_INT.fromArray(POWERS_OF_31, 0);
-    var coefficients2 = coefficients1.mul(next);
-    var coefficients3 = coefficients2.mul(next);
-    var coefficients4 = coefficients3.mul(next);
-    next = next.mul(next);
-    next = next.mul(next);
+    var next = YMM_INT.broadcast(POWERS_OF_31_BACKWARDS[0]);
+    var coefficients1 = YMM_INT.fromArray(POWERS_OF_31_BACKWARDS, 33 - 8);
+    var coefficients2 = YMM_INT.fromArray(POWERS_OF_31_BACKWARDS, 33 - 16);
+    var coefficients3 = YMM_INT.fromArray(POWERS_OF_31_BACKWARDS, 33 - 24);
+    var coefficients4 = YMM_INT.fromArray(POWERS_OF_31_BACKWARDS, 33 - 32);
     var acc1 = YMM_INT.zero();
     var acc2 = YMM_INT.zero();
     var acc3 = YMM_INT.zero();
     var acc4 = YMM_INT.zero();
-    for (int i = 0; i < data.length; i += YMM_INT.length() * 4) {
-      acc1 = acc1.add(coefficients1.mul(YMM_INT.fromArray(data, i)));
-      acc2 = acc2.add(coefficients2.mul(YMM_INT.fromArray(data, i + YMM_INT.length())));
-      acc3 = acc3.add(coefficients3.mul(YMM_INT.fromArray(data, i + 2 * YMM_INT.length())));
-      acc4 = acc4.add(coefficients4.mul(YMM_INT.fromArray(data, i + 3 * YMM_INT.length())));
+    for (int i = data.length; i - 4 * YMM_INT.length() >= 0; i -= YMM_INT.length() * 4) {
+      acc1 = acc1.add(coefficients1.mul(YMM_INT.fromArray(data, i - YMM_INT.length())));
+      acc2 = acc2.add(coefficients2.mul(YMM_INT.fromArray(data, i - 2 * YMM_INT.length())));
+      acc3 = acc3.add(coefficients3.mul(YMM_INT.fromArray(data, i - 3 * YMM_INT.length())));
+      acc4 = acc4.add(coefficients4.mul(YMM_INT.fromArray(data, i - 4 * YMM_INT.length())));
       coefficients1 = coefficients1.mul(next);
       coefficients2 = coefficients2.mul(next);
       coefficients3 = coefficients3.mul(next);
       coefficients4 = coefficients4.mul(next);
     }
-    return acc1.add(acc2).add(acc3).add(acc4).addAll();
+    return acc1.add(acc2).add(acc3).add(acc4).addAll() + coefficients1.get(7);
   }
 
 }
